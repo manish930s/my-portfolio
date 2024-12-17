d8e2b9a3254b625a58cf304dafdaa5c91cba5a32
Mansh Chauhan <manishchauhan93099@gmail.com>
2024-12-17 14:03:43 +0530

    Photo3D.tsx

--- src/components/3d/Photo3D.tsx
+++ src/components/3d/Photo3D.tsx
@@ -8,21 +8,20 @@ interface Photo3DProps {
 
 export function Photo3D({ position }: Photo3DProps) {
   const meshRef = useRef<Mesh>(null);
-  const texture = useTexture('/myimage2.png'); // Ensure this is a PNG with transparency
+  const texture = useTexture('/myimage.png'); // Ensure this is a PNG with transparency
 
   return (
     <mesh ref={meshRef} position={position}>
-      <planeGeometry args={[2.5, 3.9]} /> {/* Set size of the plane */}
+      <planeGeometry args={[2.8, 4.9]} /> {/* Set size of the plane */}
       <meshStandardMaterial
         map={texture} // Apply the texture with transparency
         color="white" // Color set to white for transparency regions
-        metalness={5} // Non-metallic effect
-        roughness={9.5} // Semi-matte finish
+        metalness={4} // Non-metallic effect
+        roughness={150.5} // Semi-matte finish
         transparent={true} // Enable transparency
-        opacity={1} // Full opacity (adjust if you want to see some transparency)
+        opacity={5} // Full opacity (adjust if you want to see some transparency)
         side={DoubleSide} // Use DoubleSide to render both sides of the plane
       />
     </mesh>
   );
 }
-
